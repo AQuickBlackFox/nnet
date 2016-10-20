8e675ac0a831878256f5020c96b48d565db7378d
Aditya Atluri <Aditya.Atluri@amd.com>
2016-10-19 21:37:06 -0500

    added proper changes to do multiprocessing and return data

--- conv_parallel.py
+++ conv_parallel.py
@@ -1,44 +1,22 @@
-"""
-Error output
-"""
-from multiprocessing import Process
+import multiprocessing
 import numpy as np
 
-numThreads = 16
+numThreads = 8
 numRows = 32000
-numCols = 2
-numOut = 3
+numCols = 3
+numOut = 2
 
 stride = numRows / numThreads
 
-np.random.seed(0)
 X = np.ones((numRows, numCols))
 W = np.ones((numCols, numOut))
 B = np.ones((numRows, numOut))
-Y = np.ones((numRows, numOut))
 
-Xpart = np.zeros((numRows/numThreads, numCols))
-Bpart = np.zeros((numRows/numThreads, numOut))
-Ypart = np.zeros((numRows/numThreads, numOut))
-
-Xout, Yout = [], []
-
-for j in range(numThreads):
-  Xout.append(Xpart)
-  Yout.append(Ypart)
-
-def add(X, B, idx):
-  for j in range(numRows*10):
-    Yout[idx] = X.dot(W) + B
+def conv(idx):
+  for i in range(100000):
+    X[idx*stride:idx*stride+stride].dot(W) + B[idx*stride:idx*stride+stride]
+  return X[idx*stride:idx*stride+stride].dot(W) + B[idx*stride:idx*stride+stride]
 
 if __name__=='__main__':
-  jobs = []
-  for i in range(numThreads):
-    p = Process(target=add, args=(X[i*stride:i*stride+stride],B[i*stride:i*stride+stride],i,))
-    jobs.append(p)
-
-  for i in range(numThreads):
-    jobs[i].start()
-
-  for i in range(numThreads):
-    jobs[i].join()
+  pool = multiprocessing.Pool(numThreads)
+  Y = pool.map(conv, range(numThreads))
