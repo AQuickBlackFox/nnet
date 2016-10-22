dfcafc588b32ef1b03afa0ce915b5dc9f5fd059f
Aditya Atluri <Aditya.Atluri@amd.com>
2016-10-21 23:21:44 -0500

    added derivate loss function

--- least_square.py
+++ least_square.py
@@ -11,6 +11,9 @@ y = np.random.randn(numRows, numCols)
 def least_square():
   return (t - y)**2
 
+def derv_loss_func():
+  return -2*(t-y)
+
 if __name__ == '__main__':
-  print least_square()[0]
+  print least_square()[0], derv_loss_func()[0]
   print (t[0][0] - y[0][0])**2
